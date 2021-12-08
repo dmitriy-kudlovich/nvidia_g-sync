bfe130f29de1bac6d1039d0995e34a8f38664ffd
Dmitriy Kudlovich <carnagery@mail.ru>
2021-12-08 22:44:17 +0300

    jquery added

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,19 @@
-const html = document.querySelector("html");
-const buttonMobile = document.querySelector("#buttonMobile");
-const navMobile = document.querySelector("#navMobile");
-const ovelay = document.querySelector("#overlay");
-const buttonStart = document.querySelector("#buttonStart");
-const formBuy = document.querySelector("#formBuy");
-const buttonClose = document.querySelector("#buttonClose");
-
-buttonMobile.addEventListener("click", () => {
-  navMobile.classList.toggle("nav--mobile--active");
-  html.classList.toggle("overflow-hidden");
-  ovelay.classList.toggle("overlay--active");
-  buttonMobile.classList.toggle("button--mobile--active");
+$("#buttonMobile").click(() => {
+  $("#navMobile").toggleClass("nav--mobile--active");
+  $("html").toggleClass("overflow-hidden");
+  $("#overlay").toggleClass("overlay--active");
+  $("#buttonMobile").toggleClass("button--mobile--active");
 });
 
-buttonStart.addEventListener("click", () => {
-  formBuy.classList.toggle("form-buy--active");
-  ovelay.classList.add("overlay--active", "z-index");
-  html.classList.toggle("overflow-hidden");
+$("#buttonStart").click(() => {
+  $("#formBuy").toggleClass("form-buy--active");
+  $("#overlay").toggleClass("overlay--active z-index");
+  $("html").toggleClass("overflow-hidden");
 });
 
-buttonClose.addEventListener("click", () => {
-  formBuy.classList.remove("form-buy--active");
-  ovelay.classList.remove("overlay--active", "z-index");
-  html.classList.remove("overflow-hidden");
+$("#buttonClose").click((event) => {
+  event.preventDefault();
+  $("#formBuy").removeClass("form-buy--active");
+  $("#overlay").removeClass("overlay--active z-index");
+  $("html").removeClass("overflow-hidden");
 });
